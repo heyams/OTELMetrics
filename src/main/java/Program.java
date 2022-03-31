b46ba451e4c93fc205a26e5d96b1ebaa529b7b9b
Helen Yang <heya@microsoft.com>
2022-03-30 18:52:42 -0700

    Validate long gauge

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -89,11 +89,22 @@ public class Program {
             .setUnit("C")
             .buildWithCallback(
                     m -> {
-                        m.record(1);
                         m.record(2, Attributes.of(AttributeKey.stringKey("thing"), "engine"));
                     });
 
         Thread.sleep(90 * 1000); // wait 90 seconds
+
+        List<MetricData> metricDataList = metricExporter.getExportedMetrics();
+        assert(metricDataList.size() == 1);
+        MetricData metricData = metricDataList.get(0);
+        assert(metricData.getData().getPoints().size() == 1);
+        Collection<LongPointData> points = (Collection<LongPointData>)metricData.getData().getPoints();
+        assert(points.size() == 1);
+        LongPointData longPointData = points.iterator().next();
+        assert(longPointData.getValue() == 2L);
+        assert(longPointData.getAttributes().get(AttributeKey.stringKey("thing")) == "engine");
+
+        metricExporter.reset();
     }
 
     private static void testDoubleCounter() throws InterruptedException {
