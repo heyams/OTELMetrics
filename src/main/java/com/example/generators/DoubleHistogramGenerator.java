d850347c7b3530cc202c0692af90746f663d779b
Helen Yang <heya@microsoft.com>
2022-04-01 08:54:16 -0700

    Add historgram generator

---
+++ src/main/java/com/example/generators/DoubleHistogramGenerator.java
@@ -0,0 +1,33 @@
+package com.example.generators;
+
+import io.opentelemetry.api.metrics.DoubleHistogram;
+import io.opentelemetry.sdk.metrics.data.HistogramPointData;
+import io.opentelemetry.sdk.metrics.data.MetricData;
+
+import java.util.Collection;
+import java.util.List;
+
+public final class DoubleHistogramGenerator extends BaseGenerator {
+
+    @Override
+    public void generateMetric() throws InterruptedException {
+        DoubleHistogram doubleHistogram = meter.histogramBuilder("testDoubleHistogram")
+                .setDescription("http.client.duration")
+                .setUnit("ms")
+                .build();
+        doubleHistogram.record(123.0);
+
+        Thread.sleep(90 * 1000); // wait 90 seconds
+
+        List<MetricData> metricDataList = metricExporter.getExportedMetrics();
+        assert(metricDataList.size() == 1);
+        MetricData metricData = metricDataList.get(0);
+        assert(metricData.getData().getPoints().size() == 1);
+        Collection<HistogramPointData> points = (Collection<HistogramPointData>)metricData.getData().getPoints();
+        assert(points.size() == 1);
+        HistogramPointData histogramPointData = points.iterator().next();
+        assert(histogramPointData.getSum() == 123.0);
+
+        metricExporter.reset();
+    }
+}
---
+++ src/main/java/com/example/generators/LongHistogramGenerator.java
@@ -0,0 +1,34 @@
+package com.example.generators;
+
+import io.opentelemetry.api.metrics.LongHistogram;
+import io.opentelemetry.sdk.metrics.data.HistogramPointData;
+import io.opentelemetry.sdk.metrics.data.MetricData;
+
+import java.util.Collection;
+import java.util.List;
+
+public final class LongHistogramGenerator extends BaseGenerator {
+
+    @Override
+    public void generateMetric() throws InterruptedException {
+        LongHistogram longHistogram = meter.histogramBuilder("testLongHistogram")
+                .ofLongs()
+                .setDescription("http.client.duration")
+                .setUnit("ms")
+                .build();
+        longHistogram.record(123L);
+
+        Thread.sleep(90 * 1000); // wait 90 seconds
+
+        List<MetricData> metricDataList = metricExporter.getExportedMetrics();
+        assert(metricDataList.size() == 1);
+        MetricData metricData = metricDataList.get(0);
+        assert(metricData.getData().getPoints().size() == 1);
+        Collection<HistogramPointData> points = (Collection<HistogramPointData>)metricData.getData().getPoints();
+        assert(points.size() == 1);
+        HistogramPointData histogramPointData = points.iterator().next();
+        assert(histogramPointData.getSum() == 123L);
+
+        metricExporter.reset();
+    }
+}
